0487c63e7c7e5bfb8a05bbe5d17e59947857457b
isabellenoelnelson <isabellenoelnelson@gmail.com>
2020-11-01 13:54:58 -0600

    nov 1 pt 2

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function crazyCaps (str) {
+    str = str.split('');
+    for (var i = 0; i < str.length; i += 2) {
+        str[i] = str.toUpperCase();
+    }
+    str = str.join('');
+    return str;
+}
