d8d2acc3e5b25b95b7b7bcf25e91e227f3b8ce97
isabellenoelnelson <isabellenoelnelson@gmail.com>
2020-11-01 12:49:36 -0600

    nov 1

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function onlyOdds(num) {
+    let zero = 0; 
+    let sum = 0;
+    var i = 1
+    if (num < 1) {
+        return zero; 
+    } else {
+        for (i = 1; i < num; i += 2) {
+            sum += i; 
+        }   
+    }
+    return sum;   
+}
