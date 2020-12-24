460fa9bf568f168be3eee9774c8240650aa608e6
isabellenoelnelson <isabellenoelnelson@gmail.com>
2020-12-24 14:44:32 -0500

    dec 24

--- workshops/13-recursion-ii/01-array-sum/array-sum.js
+++ workshops/13-recursion-ii/01-array-sum/array-sum.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function arraySum(anArr) {
+    let sum = 0;
+
+    anArr.forEach((elemOrArray) => {
+        if (Array.isArray(elemOrArray)) {
+            sum += arraySum(elemOrArray);
+        } else {
+            sum += elemOrArray;
+        }
+    })
+
+    return sum;
+}
